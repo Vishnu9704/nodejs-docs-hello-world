afdc5906424850d686ff2f4a2596598d455f48b7
Vishnu9704 <67974714+Vishnu9704@users.noreply.github.com>
2024-01-26 11:15:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // https://github.com/microsoft/Web-Dev-For-Beginners/tree/main/7-bank-project/api
 // ***************************************************************************
 
-response.end("Hello World Vishnu 1 with docker");
+response.end("Hello World Vishnu 1 with docker 1");
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
